4aa26528fc90c94b5368a7e4e8ef8faad64b586c
Mike Steder <steder@gmail.com>
2013-01-09 23:53:28 -0600

    formatting

--- routes/index.js
+++ routes/index.js
@@ -12,39 +12,19 @@ var Server = mongo.Server,
 var server = new Server('localhost', 27017, {auto_reconnect: true});
 db = new Db('afternoondb', server, {safe:true});
 
-var populateDB = function() {
-    var afternoons = [
-        {
-            name: 'talk about nodejs',
-            description: 'nodejs was used to make this site',
-            score: 3
-        },
-        {
-            name: 'talk about age gates',
-            description: 'talk about our mutual love for not allowing people to use our site',
-            score: -2
-        }];
-
-    db.collection('afternoons', function(err, collection) {
-        collection.insert(afternoons, {safe:true}, function(err, result) {
-            console.log("inserted placeholder afternoons");
-        });
-    });
-};
 
 db.open(function(err, db) {
     if(!err) {
             console.log("Connected to 'afternoondb' database");
             db.collection('afternoons', {safe:true}, function(err, collection) {
-                        if (err) {
-                                        console.log("The 'afternoons' collection doesn't exist. Creating it with sample data...");
-                                        populateDB();
-                                    }
+                if (err) {
+                    console.log("The 'afternoons' collection doesn't exist. Creating it with sample data...");
+                }
                 else {
                     console.log("The 'afternoons' collection already exists.");
-                    }
-                    });
-        }
+                }
+            });
+    }
 });
 
 
@@ -58,5 +38,4 @@ exports.index = function(req, res){
                       );
         });
     });
-
 };
