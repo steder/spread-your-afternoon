623b6b9d50d7eae92a5329a853f75d317eaa0efd
Mike Steder <steder@gmail.com>
2013-01-09 23:22:42 -0600

    making the template more dynamic

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,10 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Spread Your Afternoon' });
+  res.render('index', { title: 'Spread Your Afternoon',
+                        topics: [[1, 2, 3],
+                                 ['a', 'b', -2]
+                                 ]
+                                }
+                      );
 };
