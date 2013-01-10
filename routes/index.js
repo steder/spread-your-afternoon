6b5ef6e525c3807fd7620ca6663b12080bfc68e3
Mike Steder <steder@gmail.com>
2013-01-09 23:49:36 -0600

    adding mongodb and pulling topics from the database

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,60 @@
  * GET home page.
  */
 
+var mongo = require('mongodb');
+
+var Server = mongo.Server,
+    Db = mongo.Db,
+    BSON = mongo.BSONPure;
+
+var server = new Server('localhost', 27017, {auto_reconnect: true});
+db = new Db('afternoondb', server, {safe:true});
+
+var populateDB = function() {
+    var afternoons = [
+        {
+            name: 'talk about nodejs',
+            description: 'nodejs was used to make this site',
+            score: 3
+        },
+        {
+            name: 'talk about age gates',
+            description: 'talk about our mutual love for not allowing people to use our site',
+            score: -2
+        }];
+
+    db.collection('afternoons', function(err, collection) {
+        collection.insert(afternoons, {safe:true}, function(err, result) {
+            console.log("inserted placeholder afternoons");
+        });
+    });
+};
+
+db.open(function(err, db) {
+    if(!err) {
+            console.log("Connected to 'afternoondb' database");
+            db.collection('afternoons', {safe:true}, function(err, collection) {
+                        if (err) {
+                                        console.log("The 'afternoons' collection doesn't exist. Creating it with sample data...");
+                                        populateDB();
+                                    }
+                else {
+                    console.log("The 'afternoons' collection already exists.");
+                    }
+                    });
+        }
+});
+
+
 exports.index = function(req, res){
-  res.render('index', { title: 'Spread Your Afternoon',
-                        topics: [[1, 2, 3],
-                                 ['a', 'b', -2]
-                                 ]
+    db.collection('afternoons', function(err, collection) {
+        collection.find().toArray(function(err, items) {
+            //res.send(items);
+            res.render('index', { title: 'Spread Your Afternoon',
+                                  topics: items
                                 }
                       );
+        });
+    });
+
 };
