2fe4e1019457f97b3dcc35156236d89756f5cdd7
Mike Steder <steder@gmail.com>
2013-01-16 23:37:31 -0600

    using mongoclient to connect

--- routes/index.js
+++ routes/index.js
@@ -5,34 +5,47 @@
 
 var url = require('url');
 
-var mongo = require('mongodb');
-
-var Server = mongo.Server,
-    Db = mongo.Db,
-    BSON = mongo.BSONPure;
-
+//var mongo = require('mongodb');
+var Db = require('mongodb').Db,
+    MongoClient = require('mongodb').MongoClient,
+//    Server = require('mongodb').Server,
+//    ReplSetServers = require('mongodb').ReplSetServers,
+//    ObjectID = require('mongodb').ObjectID,
+//    Binary = require('mongodb').Binary,
+//    GridStore = require('mongodb').GridStore,
+//    Code = require('mongodb').Code,
+//    BSON = require('mongodb').pure().BSON,
+//    assert = require('assert');
+    assert = require('assert');
+//var Server = mongo.Server,
+//    Db = mongo.Db,
+//    BSON = mongo.BSONPure;
 
 var connectionUri = url.parse(process.env.MONGOHQ_URL);
 var dbName = connectionUri.pathname.replace(/^\//, '');
 
 console.log(connectionUri);
 
-var server = new Server(connectionUri.hostname, parseInt(connectionUri.port), {auto_reconnect: true});
-db = new Db(dbName, server, {safe:true});
-db.open(function(err, db) {
-    if(!err) {
-            console.log("Connected to 'afternoondb' database");
-            db.collection('afternoons', {safe:true}, function(err, collection) {
-                if (err) {
-                    console.log("The 'afternoons' collection doesn't exist. Creating it with sample data...");
-                }
-                else {
-                    console.log("The 'afternoons' collection already exists.");
-                }
-            });
-    }
-});
+db = undefined;
 
+MongoClient.connect(process.env.MONGOHQ_URL, function(err, new_db) {
+    assert.equal(null, err);
+    assert.ok(new_db != null);
+
+    db = new_db;
+    //console.log(db);
+
+    console.log("Connected to 'afternoondb' database");
+    db.collection('afternoons', {safe:true}, function(err, collection) {
+        if (err) {
+            console.log("The 'afternoons' collection doesn't exist. Creating it with sample data...");
+        }
+        else {
+            console.log("The 'afternoons' collection already exists.");
+        }
+    });
+
+});
 
 exports.index = function(req, res){
     db.collection('afternoons', function(err, collection) {
