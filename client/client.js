b3c4fb9f4f7c228e45bd70f80baa54f374f646e4
Mike Steder <steder@gmail.com>
2013-04-14 01:43:08 -0500

    completely reworked upvoting and downvoting:
    Unvoting is now working as well as we're capturing enough information to reliably
    undo a vote.

--- client/client.js
+++ client/client.js
@@ -9,12 +9,11 @@ Template.controls.events({
     },
    'click #create_topic': function(event) {
         event.preventDefault(); // prevent the form from actually submitting
-        console.log("Creating topic...");
         var new_title = document.getElementById("new_title");
         var desc = document.getElementById("description");
-        console.log("New Title:" + new_title.value);
         Topics.insert({title: new_title.value,
                        votes: 0,
+                       voters: {},
                        description: desc.value,
                        owner: Meteor.userId()
                       });
@@ -43,6 +42,56 @@ Template.topic.color_class = function() {
     }
 };
 
+Template.topic.highlight_upvoted = function() {
+    var userId = Meteor.userId();
+    var voter_key = "voters."+userId;
+    var search_key = {_id: this._id};
+    search_key[voter_key] = {$exists: 1};
+    var projection = {}
+    projection[voter_key] = 1;
+    var voters = this["voters"];
+    var vote = undefined;
+    if (voters !== undefined && voters !== null) {
+        vote = this["voters"][userId];
+    }
+
+    // $inc: {votes: vote.votes[userId] * -1}
+    if (vote === undefined) {
+        return "";
+    } else {
+        if (vote > 0) {
+            return "btn-success voted";
+        } else {
+            return "";
+        }
+    }
+};
+
+Template.topic.highlight_downvoted = function() {
+    var userId = Meteor.userId();
+    var voter_key = "voters."+userId;
+    var search_key = {_id: this._id};
+    search_key[voter_key] = {$exists: 1};
+    var projection = {}
+    projection[voter_key] = 1;
+    var voters = this["voters"];
+    var vote = undefined;
+    if (voters !== undefined && voters !== null) {
+        vote = this["voters"][userId];
+    }
+
+    // $inc: {votes: vote.votes[userId] * -1}
+    if (vote === undefined) {
+        return "";
+    } else {
+        if (vote > 0) {
+            return "";
+        } else {
+            return "btn-danger voted";
+        }
+    }
+};
+
 Template.topic.is_owner = function() {
     if (this.owner == Meteor.userId() ||
         Meteor.user().admin == true) {
@@ -64,10 +113,18 @@ Template.topics.topics = function() {
 
 Template.topics.events({
     'click .up': function() {
+        Meteor.call("unvote_topic", this);
         Meteor.call("upvote_topic", this);
     },
+    'click .up.voted': function() {
+        Meteor.call("unvote_topic", this);
+    },
     'click .down': function() {
+        Meteor.call("unvote_topic", this);
         Meteor.call("downvote_topic", this);
+    },
+    'click .down.voted': function() {
+        Meteor.call("unvote_topic", this);
     },
     'click .delete': function() {
         Topics.remove(this._id);
@@ -77,11 +134,9 @@ Template.topics.events({
         $("#accordion_"+this._id).collapse("show");
     },
     'click .save_topic': function() {
-        console.log("Saving changes...");
         event.preventDefault(); // prevent the form from actually submitting
         var edit_title = document.getElementById("edit_title_"+this._id);
         var desc = document.getElementById("edit_description_"+this._id);
-        console.log("Editing Title:" + edit_title.value);
         Topics.update({_id: this._id},
                       {$set: {title: edit_title.value,
                        description: desc.value,
--- spread.js
+++ spread.js
@@ -8,7 +8,8 @@ Topics.allow({
     insert: function (userId, topic) {
         var user = Meteor.users.findOne({_id: userId});
         if (user !== null) {
-            return user.organization == 'skinnycorp'
+            // return user.organization == 'skinnycorp'
+            return true;
         }
         return false;
     },
@@ -27,7 +28,8 @@ Topics.allow({
         return allow;
     },
     remove: function (userId, topic) {
-        return userId == topic.owner;
+        var user = Meteor.users.findOne({_id: userId});
+        return (userId == topic.owner || user.admin == true);
     },
     //fetch: ['owner']
 });
@@ -36,10 +38,37 @@ Topics.allow({
 Meteor.methods({
     upvote_topic: function(topic) {
         var userId = Meteor.userId();
-        Topics.update( {_id: topic._id, voters: {$nin: [userId]}}, {$addToSet: {voters: userId}, $inc: {votes: 1}});
+        var voter_key = "voters."+userId;
+        var search_key = {_id: topic._id}
+        search_key[voter_key] = {$exists: 0}
+        var set_value = {}
+        set_value[voter_key] = 1
+        Topics.update(search_key, {$set: set_value, $inc: {votes: 1}})
+    },
+    unvote_topic: function(topic) {
+        var userId = Meteor.userId();
+        var voter_key = "voters."+userId;
+        var search_key = {_id: topic._id}
+        search_key[voter_key] = {$exists: 1}
+
+        var unset_value = {}
+        unset_value[voter_key] = 1
+        var vote = 0;
+        if (_.has(topic, "voters")) {
+            vote = topic["voters"][userId];
+        }
+        vote = vote * -1;
+        Topics.update(search_key, {$inc: {"votes": vote}, $unset: unset_value})
     },
     downvote_topic: function(topic) {
         var userId = Meteor.userId();
-        Topics.update( {_id: topic._id, voters: {$in: [userId]}}, {$pull: {voters: userId}, $inc: {votes: -1}});
+        var voter_key = "voters."+userId;
+
+        var search_key = {_id: topic._id}
+        search_key[voter_key] = {$exists: 0}
+        var set_value = {}
+        var vote = -1;
+        set_value[voter_key] = vote;
+        Topics.update(search_key, {$set: set_value, $inc: {votes: vote}})
     },
 })
